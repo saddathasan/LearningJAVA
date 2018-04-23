9dfb700b26b954654baafff1d4c6ca6744511273
Saddat Hasan <dr.zola@hydra-machine.local>
2018-04-23 14:24:17 +0600

    second commit again

--- src/gen/ByteUShift.java
+++ src/gen/ByteUShift.java
@@ -14,6 +14,8 @@ package gen;
  * Apr 23, 2018
  *
  */
+
+//unsigned shifting a byte shift
 public class ByteUShift {
 
 	/**
@@ -21,6 +23,23 @@ public class ByteUShift {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		char hex[] = {
+				'0','1','2','3','4','5','6','7',
+				'8','9','a','b','c','d','e','f'
+		};
+		byte b = (byte) 0xf1;
+		byte c = (byte) (b >> 4);
+		byte d = (byte) (b >>> 4);
+		byte e = (byte) ((b & 0xff) >> 4);
+		
+		System.out.println("              b = 0x" + hex[(b >> 4) & 0x0f] + hex[b & 0x0f]);
+		System.out.println("         b >> 4 = 0x" + hex[(c >> 4) & 0x0f] + hex[c & 0x0f]);
+		System.out.println("        b >>> 4 = 0x" + hex[(d >> 4) & 0x0f] + hex[d & 0x0f]);
+		System.out.println("(b & 0xff) >> 4 = 0x" + hex[(e >> 4) & 0x0f] + hex[e & 0x0f]);
+		
+				
+			
 
 	}
 
